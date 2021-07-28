3235a722748f6c7c7ccf15ffd05c69c0697c0e81
alex <a@a.Fr>
2021-07-28 09:23:07 +0200

    start seleniummigration

--- src/test.py
+++ src/test.py
@@ -1,9 +1,6 @@
-import requests
-from urllib.parse import unquote, urlencode, urljoin, urlparse, quote
-from bs4 import BeautifulSoup
-import urllib3
-
-import pymongo
-
-
-print(output)
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+browser = webdriver.Firefox(executable_path="./lib/geckodriver")
+browser.get('https://www.linuxhint.com')
+print('Title: %s' % browser.title)
+browser.quit()
